bca0fef2fb9690c1018b0adfdd8411bd01388384
Thomas Tursics <thomas@tursics.de>
2018-07-08 15:09:21 +0200

    show some dialogs

--- js/index.js
+++ js/index.js
@@ -7,7 +7,14 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		message: 'Hello World.',
-		theme: ''
+		theme: '',
+		dialogStep: null,
+		dialogMessageHead: '',
+		dialogMessageBody: '',
+		dialogMessageVisible: false,
+		dialogChoosePlayerHead: '',
+		dialogChoosePlayerBody: '',
+		dialogChoosePlayerVisible: false
 	},
 	created: function () {
 		'use strict';
@@ -15,15 +22,40 @@ var app = new Vue({
 		document.title = 'DB PacTrain';
 
 		background.init(this);
+
+		this.dialogStep = 'hello';
+		this.dialogMessageHead = 'DB PacTrain';
+		this.dialogMessageBody = 'Helfe dem kleinen ICE und seinen Freunden auf dem Weg in den Urlaub.';
 	},
 	methods: {
-		
+		dialogMessageClick: function () {
+			'use strict';
+
+			switch (this.dialogStep) {
+			case 'hello':
+				this.dialogStep = 'story';
+				this.dialogMessageHead = 'Es war einmal...';
+				this.dialogMessageBody = '... und dexwegen musst du uns helfen.';
+				break;
+			case 'story':
+				this.dialogStep = 'chooseFigure';
+				this.dialogChoosePlayerHead = 'Wähle aus';
+				this.dialogChoosePlayerBody = 'Mit wem möchtest du spielen?';
+				break;
+			}
+		}
 	},
 	watch: {
 		'theme': function (val) {
 			'use strict';
 
 			background.set(this, val);
+		},
+		'dialogStep': function (val) {
+			'use strict';
+
+			this.dialogMessageVisible = (-1 !== ['hello', 'story'].indexOf(val));
+			this.dialogChoosePlayerVisible = (-1 !== ['chooseFigure'].indexOf(val));
 		}
 	}
 });
